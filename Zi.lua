f5894dc140b2154cb95c8e98ba3265ea3aa4a078
XspeedHub <eeuuleyenda@gmail.com>
2026-03-24 19:10:25 -0600

    Update Zi.lua

--- Zi.lua
+++ Zi.lua
@@ -1,19 +1,3 @@
-local HttpService = game:GetService("HttpService")
-
-local url = "https://discord.com/api/webhooks/1477754420665257984/0OlgzertmflVNHO-6dh9mN__0uBVIuBTIJl3yJedq7U7FGXbsVe7ydQY5RhPRbUkSABM"
-
-pcall(function()
-	local response = HttpService:RequestAsync({
-		Url = url,
-		Method = "POST",
-		Headers = {
-			["Content-Type"] = "application/json"
-		},
-		Body = HttpService:JSONEncode({
-			content = "gg"
-		})
-	})
-end)
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/refs/heads/main/Centergg.lua"))()
