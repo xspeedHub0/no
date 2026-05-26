25ec19438b786c5fd641651985782af4261c8202
XspeedHub <eeuuleyenda@gmail.com>
2026-05-26 09:44:24 -0600

    Update Jsjsj.lua

--- Jsjsj.lua
+++ Jsjsj.lua
@@ -9,14 +9,14 @@ task.spawn(function()
         local code = [[
             task.spawn(function()
                 pcall(function()
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/won.lua"))()
+                    -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/won.lua"))()
                 end)
             end)
         ]]
 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/won.lua"))()           
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/test"))()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/errores.lua"))()
+        --loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/won.lua"))()           
+        --loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/test"))()
+        --loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/errores.lua"))()
 
         local q = queue_on_teleport or queueonteleport
         if q then
