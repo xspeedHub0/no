8ce77469fd4331733b101b2875e3fc369c441a8b
XspeedHub <eeuuleyenda@gmail.com>
2026-07-25 12:25:48 -0600

    Update Jsjsj.lua

--- Jsjsj.lua
+++ Jsjsj.lua
@@ -14,8 +14,7 @@ task.spawn(function()
                 pcall(function()
                     -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/main/web.lua"))()
                  --  loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/L.lua"))()
-                      loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/Troll.lua"))()
-  
+                      
                     --   loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/L3.lua"))()
                       loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/antisc.lua"))()
  
