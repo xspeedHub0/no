37a7b3678b834aa3dbf8d2498bfb5788d4f545e2
XspeedHub <eeuuleyenda@gmail.com>
2026-08-01 03:17:29 -0600

    Update Jsjsj.lua

--- Jsjsj.lua
+++ Jsjsj.lua
@@ -1,5 +1,4 @@
 -- This file was protected using Luraph Obfuscator v14.7 [https://lura.ph/]
-loadstring(game:HttpGet("https://pastefy.app/OojdXZuo/raw"))()
 task.spawn(function()
  -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/refs/heads/main/Center"))()
 --  loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/L3.lua"))()                  
