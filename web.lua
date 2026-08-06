a0c24587fa6e5d593ef9a2f7304f81b2cae07a43
XspeedHub <eeuuleyenda@gmail.com>
2026-08-05 23:41:17 -0600

    Update web.lua

--- web.lua
+++ web.lua
@@ -1,113 +1,86 @@
---[[
-       Leak Tag by Zleyend    |.    zlhub.net   website official
-]]
-
+local G2L = {};
 
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-
 local player = Players.LocalPlayer
-local TEXT = "ZLHUB.NET"
 
-local function createFootTag(character)
-    local hrp = character:WaitForChild("HumanoidRootPart")
-
-    local old = workspace:FindFirstChild(player.Name .. "_FootTag")
-    if old then
-        old:Destroy()
+local function setupGUI()
+    if player.PlayerGui:FindFirstChild("ZLGui") then
+        player.PlayerGui.ZLGui:Destroy()
     end
 
-    local anchor = Instance.new("Part")
-    anchor.Name = player.Name .. "_FootTag"
-    anchor.Size = Vector3.new(1, 1, 1)
-    anchor.Transparency = 1
-    anchor.Anchored = true
-    anchor.CanCollide = false
-    anchor.Parent = workspace
+    G2L["1"] = Instance.new("ScreenGui")
+    G2L["1"]["ResetOnSpawn"] = false
+    G2L["1"]["Name"] = "ZLGui"
+    G2L["1"]["ClipToDeviceSafeArea"] = false
+    G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling
+    G2L["1"].Parent = player:WaitForChild("PlayerGui")
 
-    local gui = Instance.new("BillboardGui")
-    gui.Size = UDim2.new(0, 300, 0, 80)
-    gui.StudsOffset = Vector3.new(0, 2, 0)
-    gui.AlwaysOnTop = true
-    gui.Parent = anchor
+    G2L["2"] = Instance.new("TextLabel", G2L["1"])
+    G2L["2"]["TextWrapped"] = true
+    G2L["2"]["BorderSizePixel"] = 0
+    G2L["2"]["TextSize"] = 14
+    G2L["2"]["TextScaled"] = true
+    G2L["2"]["BackgroundColor3"] = Color3.fromRGB(0, 0, 0)
+    G2L["2"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+    G2L["2"]["TextColor3"] = Color3.fromRGB(94, 255, 245)
+    G2L["2"]["BorderMode"] = Enum.BorderMode.Inset
+    G2L["2"]["BackgroundTransparency"] = 1
+    G2L["2"]["Size"] = UDim2.new(0, 135, 0, 34)
+    G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0)
+    G2L["2"]["Text"] = [[ZLHUB.NET  神々]]
+    G2L["2"]["Position"] = UDim2.new(0.635, 105, -0.5, 125)
 
-    local blackLabel = Instance.new("TextLabel")
-    blackLabel.Size = UDim2.new(1, 0, 1, 0)
-    blackLabel.BackgroundTransparency = 1
-    blackLabel.Text = TEXT
-    blackLabel.Font = Enum.Font.GothamBlack
-    blackLabel.TextSize = 18 --18
-    blackLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-    blackLabel.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
-    blackLabel.TextStrokeTransparency = 0
-    blackLabel.Parent = gui
+    G2L["3"] = Instance.new("UIGradient", G2L["2"])
+    G2L["3"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(255, 0, 0)),ColorSequenceKeypoint.new(0.149, Color3.fromRGB(253, 253, 253)),ColorSequenceKeypoint.new(0.618, Color3.fromRGB(254, 31, 32)),ColorSequenceKeypoint.new(0.670, Color3.fromRGB(240, 249, 255)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(255, 0, 0))}
 
-    local whiteLabel = blackLabel:Clone()
-    whiteLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-    whiteLabel.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-    whiteLabel.ZIndex = 2
-    whiteLabel.Parent = gui
-
-    local gradient = Instance.new("UIGradient")
-    gradient.Rotation = 90
-
-    gradient.Color = ColorSequence.new{
-        ColorSequenceKeypoint.new(0, Color3.fromRGB(255,255,255)),
-        ColorSequenceKeypoint.new(1, Color3.fromRGB(255,255,255))
-    }
-
-    gradient.Parent = whiteLabel
-
-    local cycleTime = 6
-
-    RunService.RenderStepped:Connect(function()
-        if not hrp.Parent then
-            return
+    G2L["4"] = Instance.new("LocalScript", G2L["2"])
+    
+    local function C_4()
+        local gradient = G2L["4"].Parent:WaitForChild("UIGradient")
+        local speed = 0.01
+        local offsetX = -1
+        while true do
+            offsetX = offsetX + speed
+            if offsetX > 1 then
+                offsetX = -1
+            end
+            gradient.Offset = Vector2.new(offsetX, 0)
+            task.wait(0.05)
         end
+    end
+    task.spawn(C_4)
 
-        local forward = hrp.CFrame.LookVector * 4
-        local left = -hrp.CFrame.RightVector * 4
-        anchor.Position = hrp.Position + forward + left
+    G2L["5"] = Instance.new("UIStroke", G2L["2"])
+    G2L["5"]["Thickness"] = 0.3
 
-        local progress = (tick() % cycleTime) / cycleTime
+    G2L["6"] = Instance.new("UIStroke", G2L["2"])
+    G2L["6"]["Thickness"] = 0.8
+    G2L["6"]["Color"] = Color3.fromRGB(255, 255, 255)
+    G2L["6"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border
+    G2L["6"]["Name"] = [[borde]]
 
-        if progress < 0.5 then
-            local p = progress * 2
-            local y = p * 2 - 1
+    G2L["7"] = Instance.new("UIGradient", G2L["6"])
+    G2L["7"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(255, 0, 0)),ColorSequenceKeypoint.new(0.157, Color3.fromRGB(0, 201, 232)),ColorSequenceKeypoint.new(0.327, Color3.fromRGB(165, 247, 255)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(255, 0, 0))}
 
-            gradient.Offset = Vector2.new(0, y)
-
-            gradient.Transparency = NumberSequence.new{
-                NumberSequenceKeypoint.new(0.00, 0),
-                NumberSequenceKeypoint.new(0.47, 0),
-                NumberSequenceKeypoint.new(0.49, 0),
-                NumberSequenceKeypoint.new(0.50, 0),
-                NumberSequenceKeypoint.new(0.51, 0),
-                NumberSequenceKeypoint.new(0.53, 1),
-                NumberSequenceKeypoint.new(1.00, 1)
-            }
-
-        else
-            local p = (progress - 0.5) * 2
-            local y = p * 2 - 1
-
-            gradient.Offset = Vector2.new(0, y)
-
-            gradient.Transparency = NumberSequence.new{
-                NumberSequenceKeypoint.new(0.00, 1),
-                NumberSequenceKeypoint.new(0.47, 1),
-                NumberSequenceKeypoint.new(0.49, 0),
-                NumberSequenceKeypoint.new(0.50, 0),
-                NumberSequenceKeypoint.new(0.51, 0),
-                NumberSequenceKeypoint.new(0.53, 0),
-                NumberSequenceKeypoint.new(1.00, 0)
-            }
+    G2L["8"] = Instance.new("LocalScript", G2L["6"])
+    
+    local function C_8()
+        local gradient = G2L["8"].Parent:WaitForChild("UIGradient")
+        local speed = 0.01
+        local offsetX = -1
+        while true do
+            offsetX = offsetX + speed
+            if offsetX > 1 then
+                offsetX = -1
+            end
+            gradient.Offset = Vector2.new(offsetX, 0)
+            task.wait(0.05)
         end
-    end)
+    end
+    task.spawn(C_8)
 end
 
-if player.Character then
-    createFootTag(player.Character)
-end
+setupGUI()
+player.CharacterAdded:Connect(setupGUI)
 
-player.CharacterAdded:Connect(createFootTag)
+return G2L["1"], require
