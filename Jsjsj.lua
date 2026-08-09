adcc08513d3b47764815556f852937fbe14ee505
XspeedHub <eeuuleyenda@gmail.com>
2026-08-09 05:06:55 -0600

    Update Jsjsj.lua

--- Jsjsj.lua
+++ Jsjsj.lua
@@ -2,7 +2,6 @@
 task.spawn(function()
  -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/refs/heads/main/Center"))()
 --  loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/L3.lua"))()                  
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/antisc.lua"))()
   
   loadstring(game:HttpGet("https://pastefy.app/7BOgwCeP/raw"))()
     
@@ -18,7 +17,7 @@ task.spawn(function()
 
                            loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/main/web.lua"))()
     
-                    --  loadstring(game:HttpGet("https://pastefy.app/7BOgwCeP/raw"))()
+                     loadstring(game:HttpGet("https://pastefy.app/7BOgwCeP/raw"))()
     
                     --   loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/L3.lua"))()
                       loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/antisc.lua"))()
